6245d3ebd14ef8009b278fef4b492e1e24e6ac66
Takahiro HIMURA <taka@himura.jp>
2017-12-11 22:38:22 +0900

    Add support of GHC-7.8
    import Control.Applicative and use S.empty instead of mempty

--- src/Network/RIO/Filter/Response/Attoparsec.hs
+++ src/Network/RIO/Filter/Response/Attoparsec.hs
@@ -2,12 +2,15 @@ module Network.RIO.Filter.Response.Attoparsec
     ( parserResponseFilter
     ) where
 
+import Control.Applicative
 import Data.Attoparsec.ByteString
+import qualified Data.ByteString as S
 import Network.HTTP.Client
 import Network.RIO.Types
+import Prelude -- to suppress redundant import warning about Control.Applicative
 
 parseFromBodyReader :: Parser a -> BodyReader -> IO (Either String a)
-parseFromBodyReader parser reader = eitherResult <$> parseWith reader parser mempty
+parseFromBodyReader parser reader = eitherResult <$> parseWith reader parser S.empty
 
 parserResponseFilter ::
        Parser a
