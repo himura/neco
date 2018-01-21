943afa80e2c4e91f90329a729ec80b84ebf2ba55
Takahiro HIMURA <taka@himura.jp>
2018-01-21 17:45:19 +0900

    Add makeDummyResponse

--- src/Network/RIO/Service/Stub.hs
+++ src/Network/RIO/Service/Stub.hs
@@ -1,9 +1,23 @@
 module Network.RIO.Service.Stub
     ( stubService
+    , makeDummyResponse
     ) where
 
 import Network.HTTP.Client
+import Network.HTTP.Client.Internal
+import qualified Network.HTTP.Types as HTTP
 import Network.RIO.Types
 
 stubService :: Response a -> Service m Request (Response a)
 stubService res = Service $ \_req respond -> respond res
+
+makeDummyResponse :: body -> Response body
+makeDummyResponse body =
+    Response
+    { responseStatus = HTTP.status200
+    , responseVersion = HTTP.HttpVersion 1 1
+    , responseHeaders = []
+    , responseCookieJar = CJ []
+    , responseBody = body
+    , responseClose' = ResponseClose $ return ()
+    }
